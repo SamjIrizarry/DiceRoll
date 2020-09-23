bb85cc6bf474f8c062a2d813732bed7eea9e35da
SamjIrizarry <71662820+SamjIrizarry@users.noreply.github.com>
2020-09-23 09:32:45 -0400

    I changed the number into dots, and added an animation to it.

--- main.ts
+++ main.ts
@@ -1,6 +1,57 @@
 input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.Chessboard)
     roll = randint(1, 6)
-    basic.showNumber(roll)
+    if (roll == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    } else if (roll == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (roll == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (roll == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (roll == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
 let roll = 0
 basic.showString("Shake Me")
