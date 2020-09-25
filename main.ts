2313ace92e877eaab10a9a0fd8330e5698ca99bf
SamjIrizarry <71662820+SamjIrizarry@users.noreply.github.com>
2020-09-25 12:46:36 -0400

    I made my first draft of black jack

--- main.ts
+++ main.ts
@@ -1,57 +1,379 @@
-input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.SmallDiamond)
-    basic.showIcon(IconNames.Diamond)
-    basic.showIcon(IconNames.Chessboard)
-    roll = randint(1, 6)
-    if (roll == 6) {
+input.onPinPressed(TouchPin.P0, function () {
+    let _3rd_done = 0
+    if (_3rd_done == not_set) {
+        _3rd_Card = randint(1, 10)
+    }
+    if (_3rd_Card == 10) {
         basic.showLeds(`
-            . . . . .
-            . # . # .
-            . # . # .
-            . # . # .
-            . . . . .
+            # . . # .
+            # . # . #
+            # . # . #
+            # . # . #
+            # . . # .
             `)
-    } else if (roll == 5) {
+    } else if (_3rd_Card == 9) {
         basic.showLeds(`
-            . . . . .
+            . # # # .
             . # . # .
-            . . # . .
-            . # . # .
-            . . . . .
+            . # # # .
+            . . . # .
+            . . . # .
             `)
-    } else if (roll == 4) {
+    } else if (_3rd_Card == 8) {
         basic.showLeds(`
-            . . . . .
+            . # # # .
             . # . # .
-            . . . . .
+            . # # # .
             . # . # .
-            . . . . .
+            . # # # .
             `)
-    } else if (roll == 3) {
+    } else if (_3rd_Card == 7) {
         basic.showLeds(`
-            . . . . .
-            . . # . .
-            . . . . .
-            . # . # .
-            . . . . .
+            . # # # .
+            . . . # .
+            . . . # .
+            . . . # .
+            . . . # .
             `)
-    } else if (roll == 2) {
+    } else if (_3rd_Card == 6) {
         basic.showLeds(`
-            . . . . .
-            . . . . .
+            . # # # .
+            . # . . .
+            . # # # .
             . # . # .
-            . . . . .
-            . . . . .
+            . # # # .
+            `)
+    } else if (_3rd_Card == 5) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    } else if (_3rd_Card == 4) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # # # .
+            . . . # .
+            . . . # .
+            `)
+    } else if (_3rd_Card == 3) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    } else if (_3rd_Card == 2) {
+        basic.showLeds(`
+            . # # . .
+            . . . # .
+            . # # # .
+            . # . . .
+            . # # # .
             `)
     } else {
         basic.showLeds(`
-            . . . . .
-            . . . . .
             . . # . .
-            . . . . .
-            . . . . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    }
+    basic.pause(1000)
+    if (roll + (_2ndCard + _3rd_Card) > 21) {
+        basic.showString("Bust!")
+    }
+    if (roll + (_2ndCard + _3rd_done) == 21) {
+        basic.showString("Black Jack!")
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (roll == 10) {
+        basic.showLeds(`
+            # . . # .
+            # . # . #
+            # . # . #
+            # . # . #
+            # . . # .
+            `)
+    } else if (roll == 9) {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . # .
+            . . . # .
+            `)
+    } else if (roll == 8) {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # # # .
+            . # . # .
+            . # # # .
+            `)
+    } else if (roll == 7) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . . . # .
+            . . . # .
+            . . . # .
+            `)
+    } else if (roll == 6) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            `)
+    } else if (roll == 5) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    } else if (roll == 4) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # # # .
+            . . . # .
+            . . . # .
+            `)
+    } else if (roll == 3) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    } else if (roll == 2) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . # . . .
+            . # # # .
+            `)
+    } else if (roll == 11) {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # # # .
+            . # . # .
+            . # . # .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
             `)
     }
 })
+input.onButtonPressed(Button.B, function () {
+    if (_2ndCard == 10) {
+        basic.showLeds(`
+            # . . # .
+            # . # . #
+            # . # . #
+            # . # . #
+            # . . # .
+            `)
+    } else if (_2ndCard == 9) {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . # .
+            . . . # .
+            `)
+    } else if (_2ndCard == 8) {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # # # .
+            . # . # .
+            . # # # .
+            `)
+    } else if (_2ndCard == 7) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . . . # .
+            . . . # .
+            . . . # .
+            `)
+    } else if (_2ndCard == 6) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            `)
+    } else if (_2ndCard == 5) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    } else if (_2ndCard == 4) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # # # .
+            . . . # .
+            . . . # .
+            `)
+    } else if (_2ndCard == 3) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    } else if (_2ndCard == 2) {
+        basic.showLeds(`
+            . # # . .
+            . . . # .
+            . # # # .
+            . # . . .
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    }
+    basic.pause(1000)
+    if (roll + _2ndCard == 21) {
+        basic.showString("Black Jack!")
+    }
+})
+input.onPinPressed(TouchPin.P1, function () {
+    if (_4th_card == not_set) {
+        _4th_card = randint(1, 10)
+    }
+    if (_4th_card == 10) {
+        basic.showLeds(`
+            # . . # .
+            # . # . #
+            # . # . #
+            # . # . #
+            # . . # .
+            `)
+    } else if (_4th_card == 9) {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . # .
+            . . . # .
+            `)
+    } else if (_4th_card == 8) {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # # # .
+            . # . # .
+            . # # # .
+            `)
+    } else if (_4th_card == 7) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . . . # .
+            . . . # .
+            . . . # .
+            `)
+    } else if (_4th_card == 6) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            `)
+    } else if (_4th_card == 5) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    } else if (_4th_card == 4) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # # # .
+            . . . # .
+            . . . # .
+            `)
+    } else if (_4th_card == 3) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    } else if (_4th_card == 2) {
+        basic.showLeds(`
+            . # # . .
+            . . . # .
+            . # # # .
+            . # . . .
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    }
+    basic.pause(1000)
+    if (_2ndCard + (roll + (_3rd_Card + _4th_card)) > 21) {
+        basic.showString("Bust!")
+    }
+    if (roll + (_2ndCard + (_3rd_Card + _4th_card)) == 21) {
+        basic.showString("Black Jack!")
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 10; index++) {
+        basic.showString("stay")
+    }
+})
+let _4th_card = 0
+let not_set = 0
+let _3rd_Card = 0
+let _2ndCard = 0
 let roll = 0
-basic.showString("Shake Me")
+roll = randint(1, 11)
+_2ndCard = randint(1, 10)
+_3rd_Card = not_set
+let _4th_Done = not_set
